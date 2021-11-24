0d2ff7b0b48201dd2b5ca1d3a32001c1881533dc
Pauldada <1811426355@qq.com>
2021-11-25 01:13:23 +0800

    [4-3] 重写Task and User的List方法

--- src/main/java/com/pauldada/sakurabbs/dao/TaskRepository.java
+++ src/main/java/com/pauldada/sakurabbs/dao/TaskRepository.java
@@ -3,6 +3,11 @@ package com.pauldada.sakurabbs.dao;
 import com.pauldada.sakurabbs.entities.Task;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 //指明 数据实体的类名 和 数据实体中的ID字段的类型 !!
 public interface TaskRepository extends CrudRepository<Task,Long> {
+
+    @Override
+    public List<Task> findAll();
 }
--- src/main/java/com/pauldada/sakurabbs/dao/UserRepository.java
+++ src/main/java/com/pauldada/sakurabbs/dao/UserRepository.java
@@ -3,6 +3,11 @@ package com.pauldada.sakurabbs.dao;
 import com.pauldada.sakurabbs.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 //指明 数据实体的类名 和 数据实体中的ID字段的类型 !!
 public interface UserRepository extends CrudRepository<User,Long> {
+
+    @Override
+    public List<User> findAll();
 }
