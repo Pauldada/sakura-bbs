681980289ed3d6aef97904f2dc2425a6dbd82615
Pauldada <1811426355@qq.com>
2021-11-25 01:15:12 +0800

    [4-5] 主页控制层将List给绑定参数

--- src/main/java/com/pauldada/sakurabbs/controllers/IndexController.java
+++ src/main/java/com/pauldada/sakurabbs/controllers/IndexController.java
@@ -1,13 +1,31 @@
 package com.pauldada.sakurabbs.controllers;
 
+import com.pauldada.sakurabbs.dao.TaskRepository;
+import com.pauldada.sakurabbs.dao.UserRepository;
+import com.pauldada.sakurabbs.entities.Task;
+import com.pauldada.sakurabbs.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    TaskRepository taskRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/")
-    public String Index(){
+    public String DisplayIndex(Model model){
+        List<Task> tasks = taskRepository.findAll();
+        List<User> users = userRepository.findAll();
+        model.addAttribute("tasks",tasks);
+        model.addAttribute("users",users);
         return "index";
     }
 }
