350a108b37fde81bed422fabfea2a89f5e8c1910
Pauldada <1811426355@qq.com>
2021-11-24 23:09:10 +0800

    [3-3] 用户控制层的GET&POST请求 并 创建实体的CrudRepository

---
+++ src/main/java/com/pauldada/sakurabbs/controllers/UserController.java
@@ -0,0 +1,31 @@
+package com.pauldada.sakurabbs.controllers;
+
+import com.pauldada.sakurabbs.dao.UserRepository;
+import com.pauldada.sakurabbs.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller     //由Spring管理的控制层组件
+@RequestMapping("users")        //指定该控制层的整体路径
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/new")     //响应访问管理用户页面的GET请求的新建任务
+    public String DisplayUserForm(Model model){
+        User aUser=new User();
+        model.addAttribute("user",aUser);       //第一个参数为在HTML模板中对象的名称，第二个参数为在控制层中要传的数据
+        return "new-user";
+    }
+
+    @PostMapping("/save")   //响应网站的POST请求后的保存用户，即处理保存到数据库的行为
+    public String CreateUser(Model model,User user){
+        userRepository.save(user);          //引用接入层的save方法保存实体到数据库!!
+        return "redirect:/users/new";       //保存后回新建页面
+    }
+}
