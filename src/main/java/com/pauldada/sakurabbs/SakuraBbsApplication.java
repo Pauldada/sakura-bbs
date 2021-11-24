944790425a6cb0274e96909cbe60ce64028a29a1
Pauldada <1811426355@qq.com>
2021-11-24 21:19:56 +0800

    [1] 项目初始化

---
+++ src/main/java/com/pauldada/sakurabbs/SakuraBbsApplication.java
@@ -0,0 +1,13 @@
+package com.pauldada.sakurabbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakuraBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakuraBbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pauldada/sakurabbs/SakuraBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauldada.sakurabbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakuraBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
